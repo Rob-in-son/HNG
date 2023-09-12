e7a369ac9b51ac3022f1624d0a43fe361ace229f
Rob-in-son <estherchidinma202@gmail.com>
2023-09-13 00:13:50 +0100

    Much more progress

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ ma = Marshmallow(app)
 
 #Person class/model
 class Person(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, unique=True)
 
     def __init__(self, name):
@@ -34,16 +34,16 @@ with app.app_context():
 #Product  Schema
 class PersonSchema(ma.Schema):
     class Meta:
-        fields = ("id", "name")
+        fields = ("user_id", "name")
 
 #Init Schema
 person_schema = PersonSchema()
 persons_schema = PersonSchema(many=True)
 
-
-@app.route("/person", methods= ["POST"])
+#Adding a new person. 
+@app.route("/api", methods= ["POST"])
 def add_person():
-    name = request.json(name)
+    name = request.json['name']
     
     new_person = Person(name)
 
@@ -51,6 +51,31 @@ def add_person():
     db.session.commit()
 
     return person_schema.jsonify(new_person)
+
+#Fetching details of a person.
+@app.route("/api/<user_id>", methods= ["GET"])
+def get_persons(user_id):
+    person = Person.query.get(user_id)
+    result = person_schema.dump(person)
+    return jsonify(result)
+
+#Update person. 
+@app.route("/api/<user_id>", methods= ["PUT"])
+def update_person(user_id):
+    #fetch person
+    person = Person.query.get(user_id)
+    #get field from request body
+    name = request.json['name']
+    #Get a new person to submit to the database
+    person.name = name
+    #save
+    db.session.commit()
+
+    return person_schema.jsonify(person)
+
+
+
+
 
 # Run server
 if __name__ == "__main__":
